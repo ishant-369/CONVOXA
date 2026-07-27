a5b2fe21e5b3ed2c69c818d9354f50a18c55a368
ishant-369 <isntae@gmail.com>
2026-07-27 20:25:55 +0530

    j

--- convoxa/chats/views.py
+++ convoxa/chats/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+@api_view(["GET"])
+def chat_view(request):
+    user = request.user
+
+    return Response({
+        "username": user.username,
+        "status": "connected"
+    })
